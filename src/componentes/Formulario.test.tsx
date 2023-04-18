eab031b649cd4b10ca40aa1be0a150159967610a
Leonam Silva <leonam.murilo@gmail.com>
2023-04-17 21:52:15 -0300

    feat: Estudando teste em jest

---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
+
+test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+
+    render(<Formulario />)
+
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+    // garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+    )
+    
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+
+    // inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
+
+    // clicar no botão de submeter
+    fireEvent.click(botao)
+
+    // garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+
+    // garantir que o input não tenha um valor
+    expect(input).toHaveValue("")
+})
---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,30 @@
+import { useState, useRef } from 'react'
+import { useAdicionarParticipante } from '../state/hooks/usaAdicionarParticipante'
+
+const Formulario = () => {
+    const [nome, setNome] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const adicionarNaLista = useAdicionarParticipante()
+
+    const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        adicionarNaLista(nome)
+        setNome('')
+        inputRef.current?.focus()
+    }
+
+    return (<form onSubmit={adicionarParticipante}>
+        <input
+            ref={inputRef}
+            value={nome}
+            onChange={evento => setNome(evento.target.value)}
+            type="text"
+            placeholder="Insira os nomes dos participantes"
+        />
+        <button disabled={!nome}>Adicionar</button>
+    </form>)
+}
+
+export default Formulario
