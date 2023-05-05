fdb1c342eedff5126f5132351f66d7980e14f694
Leonam Silva <leonam.murilo@gmail.com>
2023-05-04 22:17:44 -0300

    style: ajustando formulario

--- src/componentes/Formulario.tsx
+++ src/componentes/Formulario.tsx
@@ -1,34 +1,40 @@
-import { useState, useRef } from 'react'
-import { useAdicionarParticipante } from '../state/hooks/usaAdicionarParticipante'
-import { useMensagemDeErro } from '../state/hooks/useMensagemDeErro'
+import { useState, useRef } from "react";
+import { useAdicionarParticipante } from "../state/hooks/usaAdicionarParticipante";
+import { useMensagemDeErro } from "../state/hooks/useMensagemDeErro";
+
+import "./Formulario.css";
 
 const Formulario = () => {
-    const [nome, setNome] = useState('')
+  const [nome, setNome] = useState("");
 
-    const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const adicionarNaLista = useAdicionarParticipante()
+  const adicionarNaLista = useAdicionarParticipante();
 
-    const mensagemDeErro = useMensagemDeErro()
+  const mensagemDeErro = useMensagemDeErro();
 
-    const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        adicionarNaLista(nome)
-        setNome('')
-        inputRef.current?.focus()
-    }
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    adicionarNaLista(nome);
+    setNome("");
+    inputRef.current?.focus();
+  };
 
-    return (<form onSubmit={adicionarParticipante}>
+  return (
+    <form onSubmit={adicionarParticipante}>
+      <div className="grupo-input-btn">
         <input
-            ref={inputRef}
-            value={nome}
-            onChange={evento => setNome(evento.target.value)}
-            type="text"
-            placeholder="Insira os nomes dos participantes"
+          ref={inputRef}
+          value={nome}
+          onChange={(evento) => setNome(evento.target.value)}
+          type="text"
+          placeholder="Insira os nomes dos participantes"
         />
         <button disabled={!nome}>Adicionar</button>
-        {mensagemDeErro && <p role='alert'>{mensagemDeErro}</p>}
-    </form>)
-}
+      </div>
+      {mensagemDeErro && <p role="alert">{mensagemDeErro}</p>}
+    </form>
+  );
+};
 
-export default Formulario
+export default Formulario;
