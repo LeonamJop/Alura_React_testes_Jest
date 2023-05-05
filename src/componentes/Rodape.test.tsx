f95ea7918bf065e0c0f15d28a980e4d5b76f4f8c
Leonam Silva <leonam.murilo@gmail.com>
2023-05-04 21:47:53 -0300

    test: criando teste para lista e navegação

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -1,9 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import { Rodape } from "./Rodape";
 
-describe("onde não exitem participantes suficientes", () => {
+jest.mock("../state/hooks/useListaDeParticipantes", () => {
+  return {
+    useListaDeParticipantes: jest.fn(),
+  };
+});
+
+const mockNavegacao = jest.fn();
+
+jest.mock("react-router-dom", () => {
+  return {
+    useNavigate: () => mockNavegacao,
+  };
+});
+
+describe("quando não exitem participantes suficientes", () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([]);
+  });
+
   test("a brincadeira não pode ser iniciada", () => {
     render(
       <RecoilRoot>
@@ -14,5 +32,41 @@ describe("onde não exitem participantes suficientes", () => {
     const botao = screen.getByRole("button");
 
     expect(botao).toBeDisabled();
+  });
+});
+
+describe("quando exitem participantes suficientes", () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([
+      "Ana",
+      "Catarina",
+      "Josefina",
+    ]);
+  });
+
+  test("a brincadeira pode ser iniciada", () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole("button");
+
+    expect(botao).not.toBeDisabled();
+  });
+
+  test("a brincadeira foi iniciada", () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole("button");
+    fireEvent.click(botao);
+
+    expect(mockNavegacao).toHaveBeenCalledTimes(1);
+    expect(mockNavegacao).toHaveBeenCalledWith("/sorteio");
   });
 });
