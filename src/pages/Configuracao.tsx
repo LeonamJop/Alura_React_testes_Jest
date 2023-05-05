ebfa06d1638876ad29cd6e9ff5264ce3f85800de
Leonam Silva <leonam.murilo@gmail.com>
2023-05-04 22:02:51 -0300

    feat: Criando tela de configurção

---
+++ src/pages/Configuracao.tsx
@@ -0,0 +1,13 @@
+import Formulario from "../componentes/Formulario";
+import { ListaParticipantes } from "../componentes/ListParticipantes";
+import { Rodape } from "../componentes/Rodape";
+
+export const Configuracao = () => {
+  return (
+    <>
+      <Formulario />
+      <ListaParticipantes />
+      <Rodape />
+    </>
+  );
+};
