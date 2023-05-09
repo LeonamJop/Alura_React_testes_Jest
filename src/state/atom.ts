327a2aa275fa86086815869487a64ee29c1e3db9
Leonam Silva <leonam.murilo@gmail.com>
2023-05-09 14:43:33 -0300

    feat: Criando state

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,11 +1,16 @@
-import { atom } from 'recoil'
+import { atom } from "recoil";
 
 export const listaParticipantesState = atom<string[]>({
-    key: 'listaParticipantesState',
-    default: []
-})
+  key: "listaParticipantesState",
+  default: [],
+});
+
+export const resultadoDoAmigoSecreto = atom<Map<string, string>>({
+  key: "resultadoAmigoSecreto",
+  default: new Map(),
+});
 
 export const erroState = atom<string>({
-    key: 'erroState',
-    default: ''
-})
+  key: "erroState",
+  default: "",
+});
