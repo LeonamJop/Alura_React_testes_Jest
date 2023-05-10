c929faacd48ea3aeffbeff3d58f5ce51a771a63c
Leonam Silva <leonam.murilo@gmail.com>
2023-05-09 22:02:07 -0300

    test: valida se sorteador foi chamado

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
@@ -10,6 +11,13 @@ jest.mock("../state/hooks/useListaDeParticipantes", () => {
 });
 
 const mockNavegacao = jest.fn();
+const mockSorteio = jest.fn();
+
+jest.mock("../state/hooks/useSorteador", () => {
+  return {
+    useSorteador: () => mockSorteio,
+  };
+});
 
 jest.mock("react-router-dom", () => {
   return {
@@ -68,5 +76,6 @@ describe("quando exitem participantes suficientes", () => {
 
     expect(mockNavegacao).toHaveBeenCalledTimes(1);
     expect(mockNavegacao).toHaveBeenCalledWith("/sorteio");
+    expect(mockSorteio).toHaveBeenCalledTimes(1);
   });
 });
