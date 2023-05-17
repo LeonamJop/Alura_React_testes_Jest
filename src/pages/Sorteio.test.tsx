122fcf19265d5eb600d6dbe1236ee387bdf9a861
Leonam Silva <leonam.murilo@gmail.com>
2023-05-16 21:42:27 -0300

    Test: Testando sorteador

--- src/pages/Sorteio.test.tsx
+++ src/pages/Sorteio.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import { Sorteio } from "./Sorteio";
+import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio";
 
 jest.mock("../state/hooks/useListaDeParticipantes", () => {
   return {
@@ -10,11 +11,24 @@ jest.mock("../state/hooks/useListaDeParticipantes", () => {
   };
 });
 
+jest.mock("../state/hooks/useResultadoDoSorteio", () => {
+  return {
+    useResultadoDoSorteio: jest.fn(),
+  };
+});
+
 describe("na pagina de sorteio", () => {
   const participantes = ["Ana", "Catarina", "Jorel"];
 
+  const resultado = new Map([
+    ['Ana','Jorel'],
+    ['Jorel', 'Catarina'],
+    ['Catarina','Ana']
+  ])
+
   beforeEach(() => {
     (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    (useResultadoDoSorteio as jest.Mock).mockReturnValue(resultado);
   });
 
   test("todos os participantes podem exibir o seu amigo secreto", () => {
@@ -27,4 +41,28 @@ describe("na pagina de sorteio", () => {
     const opcoes = screen.queryAllByRole("option");
     expect(opcoes).toHaveLength(participantes.length);
   });
+
+  test("o amigo secreto é exibido quando solicitado", () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+
+    const select = screen.getByPlaceholderText("Selecione o seu nome");
+
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0]
+      }
+    })
+
+    const botao = screen.getByRole('button')
+
+    fireEvent.click(botao)
+
+    const amigoSecreto = screen.getByRole('alert')
+
+    expect(amigoSecreto).toBeInTheDocument()
+  })
 });
