7a715a525c511aa193f7276fbbabc4ead0ac73ab
Leonam Silva <leonam.murilo@gmail.com>
2023-05-16 21:52:02 -0300

    Feat: Finalizando tela de sorteio

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,39 +1,50 @@
-import { useState } from "react";
-import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
-import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio";
+// src/paginas/Sorteio.tsx
+import { useState } from "react"
+import Card from "../componentes/Card"
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio"
 
-export const Sorteio = () => {
-  const participantes = useListaDeParticipantes();
-  const [participanteDaVez, setParticipanteDaVez] = useState('');
-  const [amigoSecreto, setAmigoSecreto] = useState('');
+import './Sorteio.css'
 
-  const resultado = useResultadoDoSorteio()
+const Sorteio = () => {
 
-  const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
-    evento.preventDefault();
-    if (resultado.has(participanteDaVez)) {
-      setAmigoSecreto(resultado.get(participanteDaVez)!)
+    const participantes = useListaDeParticipantes()
+
+    const [participanteDaVez, setParticipanteDaVez] = useState('')
+    const [amigoScreto, setAmigoSecreto] = useState('')
+
+    const resultado = useResultadoDoSorteio()
+
+    const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        if (resultado.has(participanteDaVez)) {
+            setAmigoSecreto(resultado.get(participanteDaVez)!)
+        }
     }
-  }
 
-  return (
-    <section>
-      <form onSubmit={sortear}>
-        <select
-          required
-          name="participantesDaVez"
-          id="participantesDaVez"
-          placeholder="Selecione o seu nome"
-          value={participanteDaVez}
-          onChange={evento => setParticipanteDaVez(evento.target.value)}
-        >
-          {participantes.map((participante) => (
-            <option key={participante}>{participante}</option>
-          ))}
-        </select>
-        <button>Sortear</button>
-      </form>
-      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-    </section>
-  );
-};
+    return (<Card>
+        <section className="sorteio">
+            <h2>Quem vai tirar o papelzinho?</h2>
+            <form onSubmit={sortear}>
+                <select
+                    required
+                    name="participanteDavez"
+                    id="participanteDavez"
+                    placeholder="Selecione o seu nome"
+                    value={participanteDaVez}
+                    onChange={evento => setParticipanteDaVez(evento.target.value)}
+                >
+                    {participantes.map(participante => <option key={participante}>{participante}</option>)}
+                </select>
+                <p>Clique em sortear para ver quem é seu amigo secreto!</p>
+                <button className="botao-sortear">Sortear</button>
+            </form>
+            {amigoScreto && <p className="resultado" role="alert">{amigoScreto}</p>}
+            <footer className="sorteio">
+                <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+            </footer>
+        </section>
+    </Card>)
+}
+
+export default Sorteio
