021a95ba35413e344cd2e45345927465915735f0
Leonam Silva <leonam.murilo@gmail.com>
2023-05-16 21:42:09 -0300

    Feat: Adicionando funções ao sorteio

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio";
 
 export const Sorteio = () => {
   const participantes = useListaDeParticipantes();
+  const [participanteDaVez, setParticipanteDaVez] = useState('');
+  const [amigoSecreto, setAmigoSecreto] = useState('');
+
+  const resultado = useResultadoDoSorteio()
+
+  const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    if (resultado.has(participanteDaVez)) {
+      setAmigoSecreto(resultado.get(participanteDaVez)!)
+    }
+  }
 
   return (
     <section>
-      <form>
-        <select name="participantesDaVez" id="participantesDaVez">
+      <form onSubmit={sortear}>
+        <select
+          required
+          name="participantesDaVez"
+          id="participantesDaVez"
+          placeholder="Selecione o seu nome"
+          value={participanteDaVez}
+          onChange={evento => setParticipanteDaVez(evento.target.value)}
+        >
           {participantes.map((participante) => (
             <option key={participante}>{participante}</option>
           ))}
         </select>
+        <button>Sortear</button>
       </form>
+      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
     </section>
   );
 };
